a1bfb21cf88df6f6dc4582b9bd9495b4719ddb32
SketchPiece <lubkin.andrey@gmail.com>
2024-08-01 16:05:02 -0700

    feat: add repl support

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,13 @@ pub use core::*;
 
 use interpreter::Interpreter;
 use parser::Parser;
+use reporter::log_reporter::LogReporter;
 use scanner::Scanner;
-use std::fs;
+use std::{
+    fs,
+    io::{self, Write},
+    rc::Rc,
+};
 
 pub fn run_file(path: &str) {
     println!("Running file: {path}");
@@ -13,13 +18,41 @@ pub fn run_file(path: &str) {
     run(&contents);
 }
 
+pub fn run_prompt() {
+    let mut line = String::new();
+    loop {
+        print!("> ");
+        let _ = io::stdout().flush();
+        line.clear();
+
+        if io::stdin().read_line(&mut line).is_err() {
+            eprintln!("Error reading input");
+            continue;
+        }
+
+        if line.trim().is_empty() {
+            break;
+        }
+
+        run(&line);
+    }
+}
+
 pub fn run(source: &str) {
-    let mut scanner = Scanner::new(source);
+    let debug_run = true;
+    let log_reporter = LogReporter::new();
+
+    let mut scanner = Scanner::new(source).attach_reporter(Rc::clone(&log_reporter));
     let tokens = scanner.scan_tokens();
-    helpers::print_tokens(&tokens);
-    let mut parser = Parser::new(tokens);
+    if debug_run {
+        helpers::print_tokens(&tokens);
+    }
+
+    let mut parser = Parser::new(tokens).attach_reporter(Rc::clone(&log_reporter));
     if let Ok(expr) = parser.parse() {
-        println!("Expression AST: {:?}", expr.stringify());
+        if debug_run {
+            println!("Expression AST: {:?}", expr.stringify());
+        }
         let interpreter = Interpreter::new(expr);
         interpreter.interpret();
     }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use std::cmp::Ordering;
 use std::env;
 use std::process;
 
 use rlox::run_file;
+use rlox::run_prompt;
 
 fn main() {
-    let script_path = env::args().nth(1).unwrap_or_else(|| {
-        eprintln!("Usage: rlox [script]");
-        process::exit(64)
-    });
-    run_file(&script_path);
+    let args_amount = env::args().count() - 1;
+
+    match args_amount.cmp(&1) {
+        Ordering::Greater => {
+            eprintln!("Usage: rlox [script]");
+            process::exit(64)
+        }
+        Ordering::Equal => {
+            let script_path = env::args().nth(1).expect("First argument must exist");
+            run_file(&script_path);
+        }
+        Ordering::Less => run_prompt(),
+    }
 }
